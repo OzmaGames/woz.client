3cda3fbb25ac2173719ca78324d8de6274997e7a
arafati <ali.prg@gmail.com>
2014-02-11 23:17:50 +0100

    game:request

--- app/api/server/events.js
+++ app/api/server/events.js
@@ -29,6 +29,7 @@
                if (callback) callback(res);
             });
          },
+
          "game:resume": function (data, callback, socket) {
             //socket.removeAllListeners("game:update");
             //socket.removeListener("game:update", update)
@@ -39,18 +40,30 @@
                app.trigger("game:start", data);
             });
          },
+
          "game:queue": function (data, callback, socket) {         
             socket.once("game:start", function (data) {
                console.log('%cgame:start', 'background: #222; color: #bada55', data);
                app.trigger("game:start", data);
             });
 
+            //game:request, type: "single"/"random"/"friend"
             if (data.friendUsername) {
-               socket.emit("game:friend", data, callback);
+               data.type = "friend";
+               //socket.emit("game:friend", data, callback);
             } else {
+               if (data.playerCount == 1) {
+                  data.type = "single";
+               } else {
+                  data.type = "random";
+               }
                delete data.friendUsername;
-               socket.emit("game:queue", data, callback);
+               //socket.emit("game:queue", data, callback);
             }
+            delete data.playerCount;
+
+            console.log(data);
+            socket.emit("game:request", data, callback);
          }
       }
    }
