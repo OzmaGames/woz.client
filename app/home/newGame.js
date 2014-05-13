a3c65357ef052e88862baf9ed0bd87213a6dfec1
arafati <ali.prg@gmail.com>
2014-05-13 14:12:55 +0200

    loc images added

--- app/home/newGame.js
+++ app/home/newGame.js
@@ -1,4 +1,4 @@
-﻿define(['durandal/app', 'api/datacontext'], function (app, ctx) {
+﻿define( ['durandal/app', 'api/datacontext'], function ( app, ctx ) {
 
    var gameOptions = [
       {
@@ -45,32 +45,32 @@
 
    vm = {
       gameOptions: gameOptions,
-      gameOptionId: ko.observable(0),
+      gameOptionId: ko.observable( 0 ),
       collections: collections,
-      collection: ko.observable('woz'),
+      collection: ko.observable( 'woz' ),
       friends: ko.observableArray(),
-      query: ko.observable(''),
-      friendListMode: ko.observable(mode.list),
-      searchLoading: ko.observable(false),
+      query: ko.observable( '' ),
+      friendListMode: ko.observable( mode.list ),
+      searchLoading: ko.observable( false ),
       activeFriend: ko.observable(),
-      friendSelected: function (friend) {
-         if (this.activeFriend() == friend) {
-            this.activeFriend(null);
-         } else if (friend.isFriend) {
-            this.activeFriend(friend);
+      friendSelected: function ( friend ) {
+         if ( this.activeFriend() == friend ) {
+            this.activeFriend( null );
+         } else if ( friend.isFriend ) {
+            this.activeFriend( friend );
          }
       },
-      queryChanged: function (e) {
-         this.query(e.target.value);
+      queryChanged: function ( e ) {
+         this.query( e.target.value );
       },
       activate: function () {
-         this.activeFriend(null);
+         this.activeFriend( null );
          app.dialog.closeAll();
-         app.trigger("game:dispose");
+         app.trigger( "game:dispose" );
 
-         app.palette.dispose();         
+         app.palette.dispose();
       },
-      collectionClicked: function (collection) {
+      collectionClicked: function ( collection ) {
          if ( collection.shortName == 'more' ) {
             app.navigate( 'shop' );
          }
@@ -79,92 +79,89 @@
       binding: function () {
          return { cacheViews: false };
       },
-      addFriend: function (friend) {
+      addFriend: function ( friend ) {
          var base = this;
-         app.trigger("server:friends", {
+         app.trigger( "server:friends", {
             username: ctx.username, command: 'add', friendUsername: friend.username
          }, function () {
-            base.query('');
-         });
+            base.query( '' );
+         } );
       },
-      removeFriend: function (friend) {
+      removeFriend: function ( friend ) {
          var base = this;
-         app.trigger("server:friends", {
+         app.trigger( "server:friends", {
             username: ctx.username, command: 'delete', friendUsername: friend.username
          }, function () {
-            if (!base.query()) {
+            if ( !base.query() ) {
                base.query.valueHasMutated();
             }
-         });
+         } );
       },
       start: function () {
-         if (vm.startEnable()) {
+         if ( vm.startEnable() ) {
             var gameOptionId = this.gameOptionId();
 
             ctx.playerCount = gameOptions[gameOptionId].playerCount;
-            
-            if (gameOptionId == 0) {
+
+            if ( gameOptionId == 0 ) {
                ctx.friendUsername = this.activeFriend().username;
             } else {
                ctx.friendUsername = "";
                delete ctx.friendUsername;
             }
 
-            ctx.collection.name(vm.collection());
+            ctx.collection.name( vm.collection() );
 
-            if ( vm.collection() != 'nf' && vm.collection() != 'woz' ) {
-               app.dialog.show("alert", { content: 'Sorry! This collection is not player yet.' });
-            } else {
-               app.navigate("game")
-            }
+            app.navigate( "game" )
+
          } else {
-            app.dialog.show("alert", { content: 'Please select a friend to continue.' });
+            app.dialog.show( "alert", { content: 'Please select a friend to continue.' } );
          }
 
       }
    }
 
-   vm.startEnable = ko.computed(function () {
+   vm.startEnable = ko.computed( function () {
       var gameOptionId = vm.gameOptionId(), activeFriend = vm.activeFriend();
       return activeFriend || gameOptionId != 0;
-   });
+   } );
 
-   ko.computed(function () {
+   ko.computed( function () {
       var query = vm.query();
-      if (query == '') {
-         app.trigger("server:friends", { username: ctx.username, command: 'getAll' }, function (data) {
-            vm.friends([]);
-            vm.activeFriend(null);
-            vm.friendListMode(mode.list);
-            if (data.success) {
-               data.friends.sort(function (a, b) { return a.username > b.username })
-               ko.utils.arrayForEach(data.friends, function (f) {
+      if ( query == '' ) {
+         app.trigger( "server:friends", { username: ctx.username, command: 'getAll' }, function ( data ) {
+            vm.friends( [] );
+            vm.activeFriend( null );
+            vm.friendListMode( mode.list );
+            if ( data.success ) {
+               data.friends.sort( function ( a, b ) { return a.username > b.username } )
+               ko.utils.arrayForEach( data.friends, function ( f ) {
                   f.isFriend = true;
-               });
-               vm.friends(data.friends);
+               } );
+               vm.friends( data.friends );
                vm.knownFriends = data.friends;
             }
-         });
+         } );
       } else {
-         vm.searchLoading(true);
-         app.trigger("server:friends", {
+         vm.searchLoading( true );
+         app.trigger( "server:friends", {
             username: ctx.username, command: 'search', friendUsername: vm.query()
-         }, function (data) {
-            ko.utils.arrayForEach(data.users, function (user) {
-               if (ko.utils.arrayFirst(vm.knownFriends, function (f) { return f.username == user.username })) {
+         }, function ( data ) {
+            ko.utils.arrayForEach( data.users, function ( user ) {
+               if ( ko.utils.arrayFirst( vm.knownFriends, function ( f ) { return f.username == user.username } ) ) {
                   user.isFriend = true;
                } else {
                   user.isFriend = false;
                }
-            });
-            vm.friends([]);
-            vm.activeFriend(null);
-            vm.friendListMode(mode.search);
-            vm.searchLoading(false);
-            vm.friends(data.users);
-         });
+            } );
+            vm.friends( [] );
+            vm.activeFriend( null );
+            vm.friendListMode( mode.search );
+            vm.searchLoading( false );
+            vm.friends( data.users );
+         } );
       }
-   }).extend({ throttle: 300 });
+   } ).extend( { throttle: 300 } );
 
    return vm;
-});
+} );
